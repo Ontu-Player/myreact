9475f2d9055bdf9e282e1084963ff33052d03ff2
salehiOntu <salehinontu@gmail.com>
2021-07-03 11:59:19 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        date: new Date(),
+      });
+    }, 1000);
+  }
+  componentWillUnmount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.state.date.toLocaleTimeString(this.props.local)}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
